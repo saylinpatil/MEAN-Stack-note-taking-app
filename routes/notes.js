74f22036d43d19067a5db1e5f8eacc55eb832ce3
Sayali Patil <123541865+saylinpatil@users.noreply.github.com>
2023-11-06 09:53:15 +0530

    Add files via upload

---
+++ routes/notes.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const router = express.Router();
+const Note = require("../models/note");
+
+// router.get('', (req, res) => {
+// res.send('footer')
+// })
+
+router.post('', (req, res) => {
+    const note = new Note({
+        title: req.body.title,
+        content: req.body.content
+    });
+    note.save().then((createdNote) => {
+        res.status(200).json({
+            message: "post added succesfully",
+            noteId: createdNote._id
+        });
+    });
+});
+
+router.get('', (req, res) => {
+    Note.find().then((document) => {
+        res.status(200).json({
+            message: "get succesfully",
+            notes: document
+        })
+    })
+});
+
+router.get('/:id', (req, res) => {
+    Note.findById(req.params.id).then((note) => {
+        console.log(note)
+        if (note) {
+            res.status(200).json(note)
+        } else {
+            res.status(404).json("note not found")
+        }
+    })
+});
+
+router.put('/:id', (req, res) => {
+    const note = new Note({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    })
+    Note.updateOne({ _id: req.params.id }, note).then(result => {
+        res.status(200).json({
+            message: "note updated successfully"
+        })
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    Note.deleteOne({ _id: req.params.id }).then(result => {
+        res.status(200).json({
+            message: "deleted succesfully"
+        })
+    })
+});
+
+module.exports = router;
